e2dc5dcc2b6e32fe4b523c2447241889c896371c
Bernardo Subercaseaux <bernardosubercaseaux@gmail.com>
2016-12-16 15:10:14 +0100

    Linear Regression first commit !
    For startint to display data

--- linear_regression.py
+++ linear_regression.py
@@ -1,1 +1,5 @@
-import pandas
+import pandas as pd
+df = pd.read_csv('train.csv')
+df.columns = ['instant','dteday','season','yr','mnth','hr','holiday','weekday','workingday','weathersit','temp','atemp','hum','windspeed','cnt']
+print df.head()
+# Inside df.head() put the number of rows you want to be shown !
